d59b562fe3a22970835c971b2fdc839ad83966b6
Harsh Singh <harshrajeevsingh@gmail.com>
2023-12-14 11:44:43 +0530

    nested comments

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useSearchParams } from "react-router-dom";
 import { closeSidebar } from "../utils/SidebarSlice";
+import CommentContainer from "./CommentContainer";
 
 const WatchPage = () => {
   const [searchParams, setSearchparams] = useSearchParams();
@@ -21,6 +22,7 @@ const WatchPage = () => {
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
       ></iframe>
+      <CommentContainer />
     </div>
   );
 };
